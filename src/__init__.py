72f3101adfe90bfef7be4e2b8e781e3046231daf
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-11-03 12:19:56 -0500

    fix: Update project version.

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-__version__="1.9.10"
+__version__="2.0.1"
 from ._core import *
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,4 @@
 import hipopybind as m
 
 def test_main():
-    assert m.__version__ == "1.9.10"
+    assert m.__version__ == "2.0.1"
